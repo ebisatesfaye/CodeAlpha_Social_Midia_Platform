90bf696b6b0cbfdf707aa019c60d598cc790421b
Eng. ebisa <ebisatesfaye30@gmail.com>
2026-02-12 23:52:58 +0300

    code updated
    Removed duplicate route definitions for Auth, User, Posts, Upload, Chat, and Message.

--- server/index.js
+++ server/index.js
@@ -32,16 +32,15 @@ app.use('/images', express.static('images'));
 dotenv.config();
 const PORT = process.env.PORT;
 
-const CONNECTION =process.env.MONGODB_CONNECTION;
-mongoose
-  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
-
-
 app.use('/auth', AuthRoute);
 app.use('/user', UserRoute)
 app.use('/posts', PostRoute)
 app.use('/upload', UploadRoute)
 app.use('/chat', ChatRoute)
 app.use('/message', MessageRoute)
+const CONNECTION =process.env.MONGODB_CONNECTION;
+mongoose
+  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
